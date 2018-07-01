059cf1f538c22b584eb6d7d0b1a6aeb122deaf45
mconway1918 <40726976+mconway1918@users.noreply.github.com>
2018-07-01 00:31:22 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,9 +3,6 @@ node default {
 node 'puppet-v01.accu.accuwx.com' {
   include role::master_server
 }
-node 'mweb-eus-z01.webserver.accuwx.com' {
-  include role::mweb_server
-}
-node 'mweb-eus-z02.webserver.accuwx.com' {
+node '/^mweb-eus-z/' {
   include role::mweb_server
 }

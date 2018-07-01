2b9fc99a62e61f33e7eb472d288e28db9a2eae4c
mconway1918 <40726976+mconway1918@users.noreply.github.com>
2018-06-30 23:02:52 -0400

    Update win2k16base.pp

--- site/profile/manifests/win2k16base.pp
+++ site/profile/manifests/win2k16base.pp
@@ -1,2 +1,6 @@
 class profile::win2k16base {
+  exec { 'Install puppet windows feature module':
+    path => 'C:/windows/system32',
+    cmd => 'cmd.exe /c puppet module install puppetlabs/windows'
+  }
 }

83fa355e6a00c4b9594708d5bd6ff27a006354d4
mconway1918 <40726976+mconway1918@users.noreply.github.com>
2018-07-01 00:11:46 -0400

    Update iis.pp

--- site/profile/manifests/iis.pp
+++ site/profile/manifests/iis.pp
@@ -1,6 +1,13 @@
 class profile::iis {
 
-  $iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net45','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework-45-ASPNET','WAS-NET-Environment','Web-Http-Redirect','Web-Filtering','Web-Mgmt-Console','Web-Mgmt-Tools']
+  $iis_features = ['FileAndStorage-Services','File-Services','FS-FileServer','FS-DFS-Replication','Storage-Services',
+         'Web-Server','Web-WebServer','Web-Common-Http','Web-Default-Doc','Web-Dir-Browsing','Web-Http-Errors','Web-Static-Content',
+         'Web-Http-Redirect','Web-Health','Web-Http-Logging','Web-Custom-Logging','Web-Log-Libraries','Web-Request-Monitor',
+         'Web-Http-Tracing','Web-Performance','Web-Stat-Compression','Web-Dyn-Compression','Web-Security','Web-Filtering',
+         'Web-IP-Security','Web-Url-Auth','Web-App-Dev','Web-Net-Ext','Web-ASP','Web-Asp-Net','Web-ISAPI-Ext','Web-ISAPI-Filter',
+         'Web-Mgmt-Tools','Web-Mgmt-Console','Web-Scripting-Tools','Web-Mgmt-Service','NET-Framework-Features','NET-Framework-Core',
+         'NET-Framework-45-Features','NET-Framework-45-Core','NET-Framework-45-ASPNET','NET-WCF-Services45','NET-WCF-TCP-PortSharing45',
+         'RSAT','RSAT-Feature-Tools','RSAT-SNMP','RSAT-Role-Tools','RSAT-File-Services','RSAT-DFS-Mgmt-Con','SNMP-Service','Telnet-Client']
 
   windowsfeature { $iis_features:
     ensure => present,
